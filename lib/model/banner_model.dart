b90d8525ac3f4b0804b67d29993e9eaf0f44a3db
sanchaman limbu <sanchaman@softechfoundation.com>
2026-08-30 16:08:56 +0545

    create new model file

---
+++ lib/model/banner_model.dart
@@ -0,0 +1,22 @@
+class BannerModel {
+  final String image;
+  final String title;
+  final String subtitle;
+  final String badge;
+
+  BannerModel({
+    required this.image,
+    required this.title,
+    required this.subtitle,
+    required this.badge,
+  });
+}
+
+class StatModel {
+  final String value;
+  final String label;
+
+  StatModel({required this.value, required this.label});
+}
+
+
