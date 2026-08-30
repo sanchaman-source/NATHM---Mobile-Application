9f5b1d9a8ef2929c27af115a2f944cd8cc08a030
sanchaman limbu <sanchaman@softechfoundation.com>
2026-08-30 16:10:40 +0545

    created new bottom nav bar folder and fle\

---
+++ lib/screen/notices/notice_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NoticesPage extends StatelessWidget {
+  const NoticesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
