b402e326fb4fdc795a1cb44b3e25116ff27b4c7a
sanchaman limbu <sanchaman@softechfoundation.com>
2026-09-01 21:10:44 +0545

    created new page file

---
+++ lib/screen/study/apply/screen/academic_step.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AcademicStep extends StatelessWidget {
+  const AcademicStep({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        
+      ],
+    );
+  }
+}
