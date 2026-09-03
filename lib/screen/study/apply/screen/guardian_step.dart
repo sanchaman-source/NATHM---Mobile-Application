a37e5e30470e48e85f676ba5bb03f050e9061e7d
sanchaman limbu <sanchaman@softechfoundation.com>
2026-09-03 17:48:15 +0545

    created new file

---
+++ lib/screen/study/apply/screen/guardian_step.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class GuardianStep extends StatelessWidget {
+  final GlobalKey<FormState> formKey;
+  const GuardianStep({super.key,
+  required this.formKey
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        
+      ],
+    );
+  }
+}
