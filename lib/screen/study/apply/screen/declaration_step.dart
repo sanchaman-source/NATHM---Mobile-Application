c64eb799123a7b521642cffbc331816ac9359e2c
sanchaman limbu <sanchaman@softechfoundation.com>
2026-09-03 17:48:06 +0545

    created new file

---
+++ lib/screen/study/apply/screen/declaration_step.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DeclarationStep extends StatelessWidget {
+  final GlobalKey<FormState> formKey;
+  const DeclarationStep({super.key,
+  required this.formKey
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        
+      ],
+    );
+  }
+}
