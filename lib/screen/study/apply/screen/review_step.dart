946693b636d50866892d5a7604f280d5e11f265a
sanchaman limbu <sanchaman@softechfoundation.com>
2026-09-03 17:48:34 +0545

    created new file

---
+++ lib/screen/study/apply/screen/review_step.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ReviewStep extends StatelessWidget {
+  final GlobalKey<FormState> formKey;
+  const ReviewStep({super.key,
+  required this.formKey
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        
+      ],
+    );
+  }
+}
