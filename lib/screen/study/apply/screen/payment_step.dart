d8a1c3fbbed09a195d31da83dd85758856bfbaf7
sanchaman limbu <sanchaman@softechfoundation.com>
2026-09-04 22:14:58 +0545

    created new file

---
+++ lib/screen/study/apply/screen/payment_step.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PaymentStep extends StatelessWidget {
+  final GlobalKey<FormState> formKey;
+  const PaymentStep({super.key,
+  required this.formKey
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        
+      ],
+    );
+  }
+}
